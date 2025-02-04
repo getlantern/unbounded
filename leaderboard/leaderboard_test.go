25e745bd93f0381996e86155e53cf2b043363e8c
Jay <110402935+jay-418@users.noreply.github.com>
2025-02-04 15:42:44 -0700

    add tests for getting all endpoints

--- leaderboard/leaderboard_test.go
+++ leaderboard/leaderboard_test.go
@@ -1,14 +1,29 @@
 package leaderboard
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestInitFromEnv(t *testing.T) {
-	creds := &supabaseCreds{}
-	err := creds.initFromEnv()
+func TestCreds(t *testing.T) {
+	client, err := new()
 	require.NoError(t, err)
-	t.Logf("loaded supabase creds for url: %s", creds.url)
+	t.Logf("loaded supabase creds for url: %s", client.url)
+}
+
+func TestRequest(t *testing.T) {
+	client, err := new()
+	require.NoError(t, err)
+
+	endpoints := []string{"teams", "connections", "team_members", "users"}
+	for _, endpoint := range endpoints {
+		t.Run(endpoint, func(t *testing.T) {
+			result, err := client.request(http.MethodGet, endpoint)
+			require.NoError(t, err)
+			require.Greater(t, len(result), 0)
+			t.Logf("%q - got %d", endpoint, len(result))
+		})
+	}
 }
