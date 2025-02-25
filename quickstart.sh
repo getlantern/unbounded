b5289b7cce797652a35b4d018473f8b0048dc6aa
Jay <110402935+jay-418@users.noreply.github.com>
2025-02-25 15:51:37 -0700

    setup typscript testing  npx jest FILEPATH

--- quickstart.sh
+++ quickstart.sh
@@ -4,16 +4,14 @@ SESSION_NAME="unbounded-sandbox"
 # FREDDIE_DEFAULT=9000
 # PORT_EGRESS_DEFAULT=8000
 
-# if [ -z "$FREDDIE" ]; then
-#     echo "PORT_FREDDIE is not set. Defaulting to $FREDDIE_DEFAULT."
-#     exit 1
-# fi
-# FREDDIE=9000
-
-# if [ -z "$PORT_EGRESS" ]; then
-#     echo "PORT_EGRESS is not set. Defaulting to $PORT_EGRESS_DEFAULT."
-#     exit 1
-# fi
+declare peers
+default_peers=2
+if [ -z "$1" ]; then
+    echo "No quantity of peers specified. Assuming default: $default_peers"
+    peers=$default_peers
+else
+    peers=$1
+fi
 
 create_tmux_session() {
     local session_name=$1
@@ -45,18 +43,17 @@ create_tmux_session() {
 
 commands=(
 
-    # start freddie
+    # start freddie for matchmaking
     "cd freddie/cmd && PORT=9000 go run main.go"
 
     # start egress
     "cd egress/cmd && PORT=8000 go run egress.go"
     
-    # start ui
+    # start ui in hot reload
     "cd ui && yarn dev:web"
 
-    # build and start native binary desktop client
-    # "cd cmd && ./build.sh desktop && cd dist/bin && FREDDIE=http://localhost:9000 EGRESS=http://localhost:8000 ./desktop"
-    # "cd cmd && ./build.sh desktop && FREDDIE=http://localhost:9000 EGRESS=http://localhost:8000 ./derek.sh 2"
+    # build and start up a number of censored peers
+    "cd cmd && ./build.sh desktop && FREDDIE=http://localhost:9000 EGRESS=http://localhost:8000 ./derek.sh $peers"
 
     # build and start native binary widget
     # "cd cmd && ./build.sh widget && cd dist/bin && FREDDIE=http://localhost:9000 EGRESS=http://localhost:8000 ./widget"
