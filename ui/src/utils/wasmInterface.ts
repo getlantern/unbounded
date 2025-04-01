06c0b41b20ab91efb2f3d494845ea1e05594e92a
Jovis <jovis@getlantern.org>
2025-04-01 20:05:34 +0800

    ui: fix wasm parameters

--- ui/src/utils/wasmInterface.ts
+++ ui/src/utils/wasmInterface.ts
@@ -78,13 +78,13 @@ declare global {
 		pTableSz: number,
 		busBufSz: number,
 		netstated: string,
+		webTransport: boolean,
 		discoverySrv: string,
 		discoverySrvEndpoint: string,
 		stunBatchSize: number,
 		tag: string,
 		egressAddr: string,
 		egressEndpoint: string,
-		webTransport: boolean
 	): WasmClient
 }
 
@@ -140,7 +140,7 @@ export class WasmInterface {
 			this.go.run(this.instance)
 			console.log('building new client')
 			this.buildNewClient()
-    }
+		}
 		this.initListeners()
 		this.handleReady()
 		this.initializing = false
@@ -157,13 +157,13 @@ export class WasmInterface {
 				WASM_CLIENT_CONFIG.pTableSz,
 				WASM_CLIENT_CONFIG.busBufSz,
 				WASM_CLIENT_CONFIG.netstated,
+				WASM_CLIENT_CONFIG.webTransport,
 				WASM_CLIENT_CONFIG.discoverySrv,
 				WASM_CLIENT_CONFIG.discoverySrvEndpoint,
 				WASM_CLIENT_CONFIG.stunBatchSize,
 				WASM_CLIENT_CONFIG.tag,
 				WASM_CLIENT_CONFIG.egressAddr,
-				WASM_CLIENT_CONFIG.egressEndpoint,
-				WASM_CLIENT_CONFIG.webTransport
+				WASM_CLIENT_CONFIG.egressEndpoint
 			)
 		}
 	}
