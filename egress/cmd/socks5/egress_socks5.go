4c45eb88094fc7704b1dbd4b7eacb514d4b49f1e
Noah Levenson <noahlevenson@gmail.com>
2026-02-21 11:33:48 -0800

    reverse TLS: make SOCKS5 and HTTP mode standalone egress servers use self-signed TLS certs at the QUIC layer

--- egress/cmd/socks5/egress_socks5.go
+++ egress/cmd/socks5/egress_socks5.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"context"
+	"crypto/rand"
+	"crypto/rsa"
 	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
+	"math/big"
 	"net"
 	"os"
 
@@ -12,6 +17,31 @@ import (
 	"github.com/getlantern/broflake/common"
 	"github.com/getlantern/broflake/egress"
 )
+
+func generateSelfSignedTLSConfig(insecureSkipVerify bool) *tls.Config {
+	key, err := rsa.GenerateKey(rand.Reader, 1024)
+	if err != nil {
+		panic(err)
+	}
+
+	template := x509.Certificate{SerialNumber: big.NewInt(1)}
+	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
+	if err != nil {
+		panic(err)
+	}
+	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
+	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
+
+	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
+	if err != nil {
+		panic(err)
+	}
+	return &tls.Config{
+		Certificates:       []tls.Certificate{tlsCert},
+		NextProtos:         []string{"broflake"},
+		InsecureSkipVerify: insecureSkipVerify,
+	}
+}
 
 func main() {
 	ctx := context.Background()
@@ -35,10 +65,7 @@ func main() {
 	common.Debugf("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n")
 
 	// And here's why it doesn't use secure TLS at the QUIC layer
-	tlsConfig := &tls.Config{
-		NextProtos:         []string{"broflake"},
-		InsecureSkipVerify: true,
-	}
+	tlsConfig := generateSelfSignedTLSConfig(true)
 
 	ll, err := egress.NewListener(ctx, l, tlsConfig)
 	if err != nil {
